b194a4ab8706574679aa7ee17930f8eb4dca8cf9
RupaliChavare7 <rupalichavare6@gmail.com>
2025-02-13 22:30:22 +0600

    Update model class -TrainingCenter.java

--- model class -TrainingCenter.java
+++ model class -TrainingCenter.java
@@ -39,4 +39,76 @@ public class TrainingCenter {
     private String contactPhone;
 
     // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCenterName() {
+        return centerName;
+    }
+
+    public void setCenterName(String centerName) {
+        this.centerName = centerName;
+    }
+
+    public String getCenterCode() {
+        return centerCode;
+    }
+
+    public void setCenterCode(String centerCode) {
+        this.centerCode = centerCode;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Integer getStudentCapacity() {
+        return studentCapacity;
+    }
+
+    public void setStudentCapacity(Integer studentCapacity) {
+        this.studentCapacity = studentCapacity;
+    }
+
+    public List<String> getCoursesOffered() {
+        return coursesOffered;
+    }
+
+    public void setCoursesOffered(List<String> coursesOffered) {
+        this.coursesOffered = coursesOffered;
+    }
+
+    public Long getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Long createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
 }
